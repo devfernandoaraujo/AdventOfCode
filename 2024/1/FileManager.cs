066c403680f1fec392290a48e38f60563f1bc74c
devfernandoaraujo <fernandomatosaraujo@gmail.com>
2025-01-11 22:18:18 -0800

    First day done

---
+++ 2024/1/FileManager.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Collections.Generic;
+namespace _1;
+
+public class FileManager
+{
+    public (List<long> LeftList, List<long> RightList) getLists(string fileName)
+    {
+        (List<long> leftList, List<long> rightList)  = mountLists(fileName);
+
+         leftList = leftList.OrderBy(x => x).ToList();
+         rightList = rightList.OrderBy(x => x).ToList();
+
+        return (leftList, rightList);
+
+    }
+
+    public List<long> similarityScore(string fileName)
+    {
+        (List<long> leftList, List<long> rightList) = mountLists(fileName);
+
+        return leftList.Select((v) => v * rightList.Where(r => r==v).Count()).ToList();
+    }
+
+    private (List<long> LeftList, List<long> RightList) mountLists(string fileName){
+        List<string> lines = new List<string>(File.ReadAllLines(fileName));
+        List<long> leftList = new List<long>(), rightList= new List<long>();   
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(' ');
+            leftList.Add(long.Parse(parts.First().Trim())); 
+            rightList.Add(long.Parse(parts.Last().Trim()));
+        }
+
+        return (leftList, rightList);
+    }
+}
---
+++ 2024/1/Program.cs
@@ -0,0 +1,15 @@
+﻿using _1;
+FileManager fileManager = new FileManager();
+
+(List<long> Left, List<long> Right) lists = fileManager.getLists("2024/1/input.txt");
+
+var total = lists.Left.Select((v, index) => Math.Abs(v - lists.Right[index]))
+    .Sum();
+
+
+Console.WriteLine($"Total elements in left list: {total}");
+
+List<long> scores = fileManager.similarityScore("2024/1/input.txt");
+
+Console.WriteLine($"Similarity score: {scores.Sum()}");
+ 
